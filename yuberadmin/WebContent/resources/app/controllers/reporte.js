3ad444c34a40343663a3666f30b884ce78c8d0c5
rodro7 <rodrigo.rossi.trabal@gmail.com>
2016-11-13 01:49:48 -0300

    hook with services remaining hook with charts

--- yuberadmin/WebContent/resources/app/controllers/reporte.js
+++ yuberadmin/WebContent/resources/app/controllers/reporte.js
@@ -1,51 +1,82 @@
-﻿(function () {
-    'use strict';
-    angular.module('yuberAdmin').controller('reporteCtrl', ['$scope', '$routeParams', 'reporteService', '$localStorage', '$location', reporteCtrl]);
+﻿(function() {
+	'use strict';
+	angular.module('yuberAdmin').controller(
+			'reporteCtrl',
+			[ '$scope', '$routeParams', 'reporteService', '$localStorage',
+					'$location', reporteCtrl ]);
 
-    function reporteCtrl($scope, $routeParams, reporteService, $localStorage, $location) {
-        if(!$localStorage.empleadoLogueado){
-            $location.url('/login');
-        }
-        
-        var proveedor = {email:null};
-        var proveedores = [];
-        $scope.showAlert = false;
+	function reporteCtrl($scope, $routeParams, reporteService, $localStorage,
+			$location) {
+		if (!$localStorage.empleadoLogueado) {
+			$location.url('/login');
+		}
 
-        var initialize = function(){
-            var id = $routeParams && $routeParams['id'] ? $routeParams['id'] : null;
-            var type = $routeParams && $routeParams['type'] ? $routeParams['type'] : null;
-            
-
-
-            reporteService.reporteRatingProv().then(function (data) {
-                console.log('reporteRatingProv')
-                $scope.proveedores = data;
+		var proveedor = {
+			email : null
+		};
+		$scope.reporteUsuariosActivos = $scope.reporteGanancia = $scope.proveedores = $scope.proveedoresPorGanancia = $scope.reporteUsuariosPorUso = $scope.reporteUsuariosPorGanancia = [];
+		$scope.showAlert = false;
+		
+		var getTimeStamp = function(date){
+			
+			return (new Date((date).toString().split(" ").splice(0, 4).join(" ")));
+		}
+		var initialize = function() {
+			var id = $routeParams && $routeParams['id'] ? $routeParams['id']
+					: null;
+			var type = $routeParams && $routeParams['type'] ? $routeParams['type']
+					: null; 
+			 
+			var nowTimestamp = getTimeStamp( new Date()).getTime();
+			var weekAgo = getTimeStamp(new Date((new Date()).setDate( new Date().getDate()-7))).getTime();
+			reporteService.reporteRatingProv(1, 10, 5).then(function(data) {
+				console.log('reporteRatingProv', data)
+				$scope.proveedores = data;
+			});
+			reporteService.reporteGanancia(0, 10, weekAgo, nowTimestamp).then(function (data) {
+                console.log('reporteGanancia', data)
+                $scope.reporteGanancia = data;
             });
-            
-        }
+			reporteService.reporteRatingProvGanancia(0, 10, weekAgo, nowTimestamp).then(function(data) {
+				console.log('reporteRatingProvGanacia', data)
+				$scope.proveedoresPorGanancia = data;
+			});
+			reporteService.reporteUsuariosPorGanancia(0, 10, weekAgo, nowTimestamp).then(function(data) {
+				console.log('reporteUsuariosPorGanancia', data)
+				$scope.reporteUsuariosPorGanancia = data;
+			});
+			reporteService.reporteUsuariosPorUso(0, 10, weekAgo, nowTimestamp).then(function(data) {
+				console.log('reporteUsuariosPorUso', data)
+				$scope.reporteUsuariosPorUso = data;
+			});
+			reporteService.reporteUsuariosActivos(0, 10, weekAgo, nowTimestamp).then(function(data) {
+				console.log('reporteUsuariosActivos', data)
+				$scope.reporteUsuariosActivos = data;
+			});
+		}
 
-        var mostrarNotificacion = function (tipo, text) {
-            var title = '';
+		var mostrarNotificacion = function(tipo, text) {
+			var title = '';
 
-            if (tipo == 'success') {
-                var title = 'Exito!';
-                text || (text = 'Acción realizada con exito.');
-            } else if (tipo == 'error') {
-                var title = 'Oh No!';
-                text || (text = 'Ha ocurrido un error.');
-            }
+			if (tipo == 'success') {
+				var title = 'Exito!';
+				text || (text = 'Acción realizada con exito.');
+			} else if (tipo == 'error') {
+				var title = 'Oh No!';
+				text || (text = 'Ha ocurrido un error.');
+			}
 
-            new PNotify({
-                title: title,
-                text: text,
-                type: tipo,
-                nonblock: {
-                    nonblock: true
-                }
-            });
-        }
-        
-        initialize();
-    }
+			new PNotify({
+				title : title,
+				text : text,
+				type : tipo,
+				nonblock : {
+					nonblock : true
+				}
+			});
+		}
+
+		initialize();
+	}
 
 })();
--- yuberadmin/WebContent/resources/app/services/reporte.js
+++ yuberadmin/WebContent/resources/app/services/reporte.js
@@ -1,27 +1,107 @@
-﻿(function () {
-   'use strict';
-   angular.module('yuberAdmin').service('reporteService', ["$http", "$q","CONFIG", reporteService]);
+﻿(function() {
+	'use strict';
+	angular.module('yuberAdmin').service('reporteService',
+			[ "$http", "$q", "CONFIG", reporteService ]);
 
-   function reporteService($http, $q, CONFIG) {
+	function reporteService($http, $q, CONFIG) {
 
-       var reporteRatingProv = function(){
-           var defer = $q.defer();
-    
-           $http.get(CONFIG.URL + '/vertical/reporteratingprov/1/10/5')
-            .success(function (reporte) {
-                defer.resolve(reporte);
-            })
-            .error(function(){
-                defer.reject('server error')
-            });
-        
-            return defer.promise;
-        };
-        
-    return {
-           reporteRatingProv  : reporteRatingProv,
-       }
+		var reporteRatingProv = function(page, amount, cal) {
+			var defer = $q.defer();
 
-   }
+			$http.get(
+					CONFIG.URL + '/vertical/reporteratingprov/' + page + '/'
+							+ amount + '/' + cal).success(function(reporte) {
+				defer.resolve(reporte);
+			}).error(function() {
+				defer.reject('server error')
+			});
+
+			return defer.promise;
+		};
+
+		var reporteUsuariosActivos = function(page, amount, start) {
+			var defer = $q.defer();
+
+			$http.get(
+					CONFIG.URL + '/vertical/rankinactusers/' + page + '/'
+							+ amount + '/' + start).success(
+					function(reporte) {
+						defer.resolve(reporte);
+					}).error(function() {
+				defer.reject('server error')
+			});
+
+			return defer.promise;
+		};
+
+		var reporteUsuariosPorUso = function(page, amount, start, end) {
+			var defer = $q.defer();
+
+			$http.get(
+					CONFIG.URL + '/vertical/rankinusuariosporconsumo/' + page
+							+ '/' + amount + '/' + start + '/' + end).success(
+					function(reporte) {
+						defer.resolve(reporte);
+					}).error(function() {
+				defer.reject('server error')
+			});
+
+			return defer.promise;
+		};
+
+		var reporteUsuariosPorGanancia = function(page, amount, start, end) {
+			var defer = $q.defer();
+
+			$http.get(
+					CONFIG.URL + '/vertical/rankingananciausuario/' + page
+							+ '/' + amount + '/' + start + '/' + end).success(
+					function(reporte) {
+						defer.resolve(reporte);
+					}).error(function() {
+				defer.reject('server error')
+			});
+
+			return defer.promise;
+		};
+
+		var reporteRatingProvGanancia = function(page, amount, start, end) {
+			var defer = $q.defer();
+
+			$http.get(
+					CONFIG.URL + '/vertical/rankingananciaprov/' + page + '/'
+							+ amount + '/' + start + '/' + end).success(
+					function(reporte) {
+						defer.resolve(reporte);
+					}).error(function() {
+				defer.reject('server error')
+			});
+
+			return defer.promise;
+		};
+
+		var reporteGanancia = function( start) {
+			var defer = $q.defer();
+
+			$http.get(
+					CONFIG.URL + '/vertical/reporteganancia/' + start).success(
+					function(reporte) {
+						defer.resolve(reporte);
+					}).error(function() {
+				defer.reject('server error')
+			});
+
+			return defer.promise;
+		};
+
+		return {
+			reporteRatingProv : reporteRatingProv,
+			reporteUsuariosActivos : reporteUsuariosActivos,
+			reporteGanancia : reporteGanancia,
+			reporteRatingProvGanancia : reporteRatingProvGanancia,
+			reporteUsuariosPorGanancia : reporteUsuariosPorGanancia,
+			reporteUsuariosPorUso : reporteUsuariosPorUso
+		}
+
+	}
 
 })();
